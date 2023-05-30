664c198f1f8bd34b9325af337e80c8d7225d664d
Cynthia-Mandere <cynthiamandere@gmail.com>
2023-05-30 13:08:40 +0300

    finalized on styling components

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Navbar/Home';
+import Menu from './components/Navbar/Menu';
+import About from './components/Navbar/About'
+import Contact from './components/Navbar/Contact'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+
+render() {
+	return (
+    
+		
+    <BrowserRouter>
+    <Routes>
+      <Route exact path='/' element = { <Home />}/>
+      <Route path='/menu' element = { <Menu />}/>
+      <Route path='/about' element = { <About />} />
+      <Route path = '/contact' element = { <Contact />}/>
+      
+    </Routes>
+    </BrowserRouter>
+     
+
+	);
+}
 }
 
 export default App;
+
