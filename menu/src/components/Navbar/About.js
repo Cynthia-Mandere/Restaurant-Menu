133bc2e2e99b5cd62f890e602b7d5f5962c7f6aa
Cynthia-Mandere <cynthiamandere@gmail.com>
2023-05-30 13:06:54 +0300

    created about component

---
+++ menu/src/components/Navbar/About.js
@@ -0,0 +1,37 @@
+import React from "react";
+//import MultiplePizzas from "url('https://as1.ftcdn.net/v2/jpg/04/94/00/04/1000_F_494000438_gu8W4LMiw24x0t1IBOFo2pg2BXLCznbq.jpg')";
+import "../styles/About.css";
+import Navbar from "./Navbar";
+function About() {
+  return (
+    <>
+    <Navbar />
+    <div className="about">
+      <div
+        className="aboutTop"
+      ></div>
+      <div className="aboutBottom">
+        <h1> ABOUT US</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
+          officiis modi at sunt excepturi expedita sint? Sed quibusdam
+          recusandae alias error harum maxime adipisci amet laborum.
+          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
+          cumque velit
+        </p>
+      </div>
+    </div>
+    </>
+    
+  );
+}
+
+export default About;
